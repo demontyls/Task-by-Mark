f0072de4ea52922afd1b6b3ef5a43b177d220350
oleg <o.zagoruy@lms-service.ru>
2022-05-19 18:23:06 +0300

    fix tab

--- task-sort/script.js
+++ task-sort/script.js
@@ -3,19 +3,18 @@
 let array = [12, 7, 92, 5, 18, 4, 32, 48, 11, 74];
 
 let newArray = [];
-let inversionNum = 0
+let inversionNum = 0;
 let n = 0;
-for(let i = 0; i < array.length; i++) {
-     
-       for( let j = 0; j < array.length; j++) {
-        if(array[j] > array[1 + j]) {
-          n = array[j];
-          array[j] = array[1 + j];
-          array[1 +j] = n;
-          inversionNum += 1
-        }  
-     }
-  console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] > array[1 + j]) {
+      n = array[j];
+      array[j] = array[1 + j];
+      array[1 + j] = n;
+      inversionNum += 1;
+    }
+  }
 }
 console.log(inversionNum);
 console.log(array);
